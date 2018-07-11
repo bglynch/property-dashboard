03c0299ea36c46abc1576dd954ff3949f854d1e4
Brian Lynch <bglynch17@gmail.com>
2018-07-11 11:14:25 +0000

    property areas chart added

--- static/js/graph.js
+++ static/js/graph.js
@@ -3,5 +3,21 @@ queue()
     .await(makeGraphs);
     
 function makeGraphs(error, propertyData){
+    var ndx = crossfilter(propertyData);
     
+    show_areas_of_properties(ndx);
+    
+    dc.renderAll();
+}
+
+function show_areas_of_properties(ndx){
+    var dim = ndx.dimension(dc.pluck('area'));
+    var group = dim.group();
+    
+    var chart = dc.rowChart("#areas_of_properties")
+        .width(600)
+        .height(330)
+        .dimension(dim)
+        .group(group)
+        .xAxis().ticks(4);
 }
